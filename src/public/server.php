b2a330567280cb0cdec90e1bd8ec23673378a564
liaol <yicoliao@gmail.com>
2016-04-29 01:03:16 +0800

    add weixin signature

--- src/public/server.php
+++ src/public/server.php
@@ -1,5 +1,31 @@
 <?php
 
+function checkSignature()
+{
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+
+    $token = TOKEN;
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+
+    if( $tmpStr == $signature ){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+if (!empty($_GET['echostr'])) {
+    if (checkSignature()) {
+        echo $_GET['echostr'];
+    } 
+    die();
+}
+
 //error_reporting(0);
 include_once ('../function.php');
 
