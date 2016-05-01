8f844b607eb23d5e1ec6c3daa58e08b921b018a2
liaol <yicoliao@gmail.com>
2016-05-01 17:34:00 +0800

    debug

--- src/config/config.php
+++ src/config/config.php
@@ -1,10 +1,10 @@
 <?php
 	$config = array(
 		'jxglurl' => 'http://210.34.213.87/',	
-		'mysql_host'=>getenv('MYSQL_PORT_3306_TCP_ADDR'),
-		'mysql_port'=>getenv('MYSQL_PORT_3306_TCP_PORT'),
-		'mysql_user'=>getenv('MYSQL_USERNAME'),
-		'mysql_pass'=>getenv('MYSQL_PASSWORD'),
-		'mysql_db'=>getenv('MYSQL_INSTANCE_NAME'),
+		'mysql_host'=>getenv("MYSQL_PORT_3306_TCP_ADDR"),
+		'mysql_port'=>getenv("MYSQL_PORT_3306_TCP_PORT"),
+		'mysql_user'=>getenv("MYSQL_USERNAME"),
+		'mysql_pass'=>getenv("MYSQL_PASSWORD"),
+		'mysql_db'=>getenv("MYSQL_INSTANCE_NAME"),
 	);
 ?>
