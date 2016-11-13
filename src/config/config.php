26f7600cdde69196840c98992f9d328faf9ea2f6
liushidong2015 <liushidong1992@vip.qq.com>
2016-11-13 23:19:14 +0800

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
 	$config = array(
-		'jxglurl' => 'http://210.34.213.87/',	
+		'jxglurl' => 'http://113.9.158.130/',	
 		'mysql_host'=>getenv('MYSQL_PORT_3306_TCP_ADDR'),
 		'mysql_port'=>getenv('MYSQL_PORT_3306_TCP_PORT'),
 		'mysql_user'=>getenv('MYSQL_USERNAME'),
